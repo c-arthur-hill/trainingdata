533fc1ddee288fb7c6518b208f01f8a28f06a45a
calvin <c.arthur.hill@gmail.com>
2019-04-21 01:23:15 +0000

    getting email field width right

--- trainingdata/users/views.py
+++ trainingdata/users/views.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from django.contrib.auth import get_user_model
@@ -6,3 +7,8 @@ class EmailView(CreateView):
     model = get_user_model()
     fields = ['email']
     success_url = '/calendar/view/today/'
+
+    def get_form(self, form_class=None):
+        form = super(EmailView, self).get_form(form_class)
+        form.fields['email'].widget = forms.EmailInput(attrs={'class': "col-9 col-xl-4"})
+        return form

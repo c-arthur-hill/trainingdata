8af02e7a9e510132b1d37daa997d8dbe1aef4a94
calvin <c.arthur.hill@gmail.com>
2019-04-23 03:51:48 +0000

    refactored front page

--- trainingdata/users/views.py
+++ trainingdata/users/views.py
@@ -5,10 +5,11 @@ from django.contrib.auth import get_user_model
 
 class EmailView(CreateView):
     model = get_user_model()
-    fields = ['email']
+    fields = ['email', 'password']
     success_url = '/calendar/view/today/'
 
     def get_form(self, form_class=None):
         form = super(EmailView, self).get_form(form_class)
         form.fields['email'].widget = forms.EmailInput(attrs={'class': "col-9 col-xl-4"})
+        form.fields['password'].widget = forms.PasswordInput(attrs={'class': "col-9 col-xl-4"})
         return form

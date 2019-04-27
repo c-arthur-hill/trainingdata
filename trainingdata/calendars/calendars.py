035b4309c7495c36ba972e3b3c875535e10abd1a
calvin <c.arthur.hill@gmail.com>
2019-04-27 14:50:00 +0000

    successfully uploading and parsing .fit files now

--- trainingdata/calendars/calendars.py
+++ trainingdata/calendars/calendars.py
@@ -1,6 +1,38 @@
 from calendar import HTMLCalendar
 from .models import DailyData
 
+#https://alexpnt.github.io/2017/07/15/django-calendar/
 class TrainingData(HTMLCalendar):
-    def __init(self):
+    def __init__(self, data=None):
+        super(TrainingData, self).__init__()
         self.data = []
+
+    def formatday(self, day, weekday, events):
+        data_html = "<ul>"
+        for event in self.data:
+            data_html += " test <br>"
+        data_html += "</ul>"
+        
+        if day == 0:
+            return '<td class="noday">&nbsp;</td>'
+        else:
+            return '<td class="%s">%d%s</td>' % (self.cssclasses[weekday], day, data_html)
+
+    def formatweek(self, theweek, data):
+        s = ''.join(self.formatday(d, wd, self.data) for (d, wd) in theweek)
+        return '<tr>%s</tr>' % s
+
+    def formatmonth(self, theyear, themonth, withyear=True):
+        month = []
+        month.append('<table border="0" cellpadding="0" cellspacing="0" class="table">')
+        month.append('\n')
+        month.append(self.formatmonthname(theyear, themonth, withyear=withyear))
+        month.append('\n')
+        month.append(self.formatweekheader())
+        month.append('\n')
+        for week in self.monthdays2calendar(theyear, themonth):
+            month.append(self.formatweek(week, self.data))
+            month.append('\n')
+        month.append('</table>')
+        month.append('\n')
+        return ''.join(month)

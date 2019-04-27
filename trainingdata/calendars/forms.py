b4ada6392710685de30a4196d46395d84f83975b
calvin <c.arthur.hill@gmail.com>
2019-04-27 19:37:03 +0000

    removing upload file

--- trainingdata/calendars/forms.py
+++ trainingdata/calendars/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import FitFile
 
 class FitFileForm(forms.ModelForm):
-    uploaded_file = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
+    uploaded_file = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}), label='')
 
     class Meta:
         model = FitFile
